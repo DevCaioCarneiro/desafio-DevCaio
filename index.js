551aaad9b15dc94c373a6da042b5a7225afaa1e5
DevCaio.C <167884939+DevCaioCarneiro@users.noreply.github.com>
2024-10-09 19:22:17 -0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+function classificadorDeHeroi() {
+    // Coletar o nome do herói
+    let nome = prompt("Digite o nome do herói:");
+
+    // Coletar a quantidade de experiência (XP)
+    let xp = parseInt(prompt("Digite a quantidade de experiência (XP) do herói:"));
+
+    // Variável para armazenar o nível do herói
+    let nivel;
+
+    // Estrutura de decisão para classificar o herói com base na XP
+    if (xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp >= 1001 && xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp >= 5001 && xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp >= 7001 && xp <= 8000) {
+        nivel = "Platina";
+    } else if (xp >= 8001 && xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Radiante";
+    }
+
+    // Exibir a mensagem com o nome e o nível do herói
+    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+}
